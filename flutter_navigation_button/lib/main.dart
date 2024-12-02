f12a0f6f73c287579967ad2c3f95f2664587d09e
Ericson313 <ericsonsaputra313@gmail.com>
2024-12-02 11:00:01 +0700

    3

--- flutter_navigation_button/lib/main.dart
+++ flutter_navigation_button/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_navigation_button/screens/favorite_screen.dart';
+import 'package:flutter_navigation_button/screens/main_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,6 +11,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: HomeScreen());
+    return const MaterialApp(home: MainScreen());
   }
 }
